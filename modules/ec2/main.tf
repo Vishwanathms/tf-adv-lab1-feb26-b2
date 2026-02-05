442b82aa8b7a83b0d954bf382f7eb30c25f923f7
vishwa <vishwa@g.com>
2026-02-05 19:15:28 +0000

    somecahnges

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this" {
   ami           = data.aws_ami.al2.id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  
+
   tags = merge({ Name = var.name }, var.tags)
 }
 
